74ef5e880f89514f0a6132e412428d64f936d6c0
Michael <Magawrych85@gmail.com>
2024-11-19 13:12:04 -0500

    added dependencies, .gitignore and .env files

--- server.mjs
+++ server.mjs
@@ -2,3 +2,11 @@ import express from 'express';
 
 const app = express()
 const PORT = 3000
+
+app.get("/", (req, res) => {
+    res.send(`<h1>Hello Ashley</h1>`);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
