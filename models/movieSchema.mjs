671efc71ebbe6eef8c0c6548ffdda119e3d61682
Michael <Magawrych85@gmail.com>
2024-11-21 15:32:15 -0500

    created movieSchema

---
+++ models/movieSchema.mjs
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const movieSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, },
+    title: { type: String, required: true, },
+    genre: { type: [String], required: true, }, //Array of strings for multiple genres
+    poster: { type: String, required: true, },
+    year: { type: Number, required: true, },
+    createdAt: { type: Date, defatilt: Date.now, },
+
+})
--- server.mjs
+++ server.mjs
@@ -1,12 +1,17 @@
 import express from 'express';
 
-const app = express()
-const PORT = 3000
+
+//Initialize app variable with Express
+const app = express();
+
 
 app.get("/", (req, res) => {
     res.send(`<h1>Hello Ashley</h1>`);
   });
   
+
+  const PORT = process.env.PORT || 3000;
+
   app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
   });
